48faaaceeae470f74079911b94e37ad0b3df1f0a
EmiliaCardenas <a01706807@tec.mx>
2025-11-26 17:30:00 -0600

    Añadir modelo, pestañas

---
+++ app/src/main/java/com/example/examen/domain/model/Modelo.kt
@@ -0,0 +1,9 @@
+package com.example.examen.domain.model
+
+
+// Poner en String un ? si es necesario que este vacio
+data class Modelo(
+    /**
+     * val nombre: Tipo de valor (String, Int)
+     * */
+)
